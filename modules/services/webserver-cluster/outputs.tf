e96320236c8115c33151d11ab90c5e2c210129f6
yekhaung <yekhaung.dev@gmail.com>
2019-12-07 21:26:38 +0630

    add outputs

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -2,3 +2,13 @@ output "alb_dns_name" {
   value       = aws_lb.alb.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "asg_name" {
+  value       = aws_autoscaling_group.webserver_asg.name
+  description = "The name of the Auto Scaling Group"
+}
+
+output "alb_security_group_id" {
+  value       = aws_security_group.alb.id
+  description = "The ID of the Security Group attached to the load balancer"
+}
